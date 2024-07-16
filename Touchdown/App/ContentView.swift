f86484501216f41838a7cf13568db9316e25d95a
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-07-16 14:18:47 -0500

    First Commit, custom Navigation

---
+++ Touchdown/App/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Oscar David Myerston Vega on 15/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                Spacer()
+                FooterView()
+                    .padding(.horizontal)
+            }//: VSTACK
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        }//: ZSTACK
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Oscar David Myerston Vega on 15/07/24.
+//
+
+import SwiftUI
+
+
+// DATA
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
+
+
