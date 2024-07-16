e2f0002533bbdccebe87d6d325eb3033f3bfb993
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-07-15 19:48:44 -0500

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Oscar David Myerston Vega on 15/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
