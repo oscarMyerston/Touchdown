2d9dd4f5efea1098fd493924e141450c8fd342f6
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-07-24 14:20:11 -0500

    Feature: Add Product Detail parte 2

--- Touchdown/App/ProductDetailView.swift
+++ Touchdown/App/ProductDetailView.swift
@@ -21,16 +21,39 @@ struct ProductDetailView: View {
             // DETAIL TOP PART
             TopPartDetailView()
                 .padding(.horizontal)
+                .zIndex(1)
 
             // DETAIL BOTTOM PART
-            // RATINGS + SIZES
-            // DESCRIPTION
-            // QUANTITY + FAVOURITE
+            VStack(alignment: .center, spacing: 0) {
 
-            // ADD TO CART
-            Spacer()
+                // RATINGS + SIZES
+                RatingsSizesDetailView()
+                    .padding(.top, -20)
+                    .padding(.bottom, 10)
+                // DESCRIPTION
+                ScrollView(.vertical, showsIndicators: false) {
+                    Text(sampleProduct.description)
+                        .font(.system(.body, design: .rounded))
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.leading)
+                }//: SCROLL
+
+                // QUANTITY + FAVOURITE
+                QuantityFavouriteDetailView()
+                    .padding(.vertical, 10)
+                // ADD TO CART
+                AddToCardDetailView()
+                    .padding(.vertical, 10)
+            }//: VSTACK
+            .padding(.horizontal)
+            .background(
+                Color.white
+                    .clipShape(CustomShape())
+                    .padding(.top, -105)
+            )
 
         }//: VSTACK
+        .zIndex(0)
         .ignoresSafeArea(.all, edges: .all)
         .background(
             Color(red: sampleProduct.red, green: sampleProduct.green, blue: sampleProduct.blue)
---
+++ Touchdown/View/Detail/AddToCardDetailView.swift
@@ -0,0 +1,41 @@
+//
+//  AddToCardDetailView.swift
+//  Touchdown
+//
+//  Created by Oscar David Myerston Vega on 24/07/24.
+//
+
+import SwiftUI
+
+struct AddToCardDetailView: View {
+    var body: some View {
+        Button {
+
+        } label: {
+            Spacer()
+            Text("Add to cart".uppercased())
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            Spacer()
+        }//: BUTTON
+        .padding(15)
+        .background(
+            Color(
+                red: sampleProduct.red,
+                green: sampleProduct.green,
+                blue: sampleProduct.blue
+            )
+        )
+        .clipShape(Capsule())
+
+    }
+}
+
+struct AddToCardDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddToCardDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
